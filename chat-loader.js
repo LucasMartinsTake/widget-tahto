708b9671d490b874cc2ec1a64cff4e375507fd9c
Take-Tahto <109627390+Take-Tahto@users.noreply.github.com>
2022-07-19 18:17:37 -0300

    Update chat-loader.js

--- chat-loader.js
+++ chat-loader.js
@@ -4,7 +4,7 @@
             .withAppKey('Y2hhdG5vc2l0ZXJvdXRlcjpkODhmYzgzZS1kNjVmLTQ5OGUtYjA2Yy0xNzk5Y2UzNmVhMTM=')
             .withButton({ "color": "#009E0B", "icon": "https://blipmediastore.blob.core.windows.net/public-medias/Media_acfae018-9432-47a8-8872-c5bde8576d52" })
             .withCustomCommonUrl('https://berlin.chat.blip.ai/')
-            .withCustomMessageMetadata({ OiDatalayer: JSON.stringify(document.OiDatalayer) })
+            .withCustomMessageMetadata({ OiDatalayer: JSON.stringify(OiDatalayer) })
             .build();
     }
 })();
